abbf11de5c1a79ab9e14578d79337ad39f4062ea
Masatooo0 <masato.n.0321@gmail.com>
2021-07-03 11:59:30 +0000

    [add]modelアソシエーション

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,10 @@
 class History < ApplicationRecord
+  validates :title, presence: true
+  validates :event, presence: true
+  validates :motivation, presence: true
+
+  has_many :reasons, dependent: :destroy
+  has_one :period, dependent: :destroy
+  accepts_nested_attributes_for :reasons, allow_destroy: true
+  accepts_nested_attributes_for :period, allow_destroy: true
 end
