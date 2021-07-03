3c6815b736dce4b67500239ccacffc6df3420c04
Masatooo0 <masato.n.0321@gmail.com>
2021-07-03 11:17:14 +0000

    [add]User deviseセットアップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+
+  protected
+# ======devise userのストロングパラメータ=======
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name,
+      :last_name,
+      :birth_day,
+      :is_valid
+      ])
+  end
+
 end
