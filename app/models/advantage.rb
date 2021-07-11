bc55810e073191ef08d6596466c5c377b0899672
Masatooo0 <masato.n.0321@gmail.com>
2021-07-11 02:40:08 +0000

    [add]バリテーション,アソシエーション

--- app/models/advantage.rb
+++ app/models/advantage.rb
@@ -1,2 +1,5 @@
 class Advantage < ApplicationRecord
+  has_many :histories
+  validates :history_id, presence: true
+  validates :advantage, presence: true
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,7 @@
 class History < ApplicationRecord
   belongs_to :user
   belongs_to :period
+  belongs_to :advantage
   has_many :reasons, inverse_of: :history
   accepts_nested_attributes_for :reasons, reject_if: :all_blank, allow_destroy: true
 end
