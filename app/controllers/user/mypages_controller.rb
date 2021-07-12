c35581f11e6724b93e6a4d95f5c6e8b19bec8864
Masatooo0 <masato.n.0321@gmail.com>
2021-07-12 12:32:59 +0000

    [add]mypageコメント

--- app/controllers/user/mypages_controller.rb
+++ app/controllers/user/mypages_controller.rb
@@ -1,5 +1,6 @@
 class User::MypagesController < ApplicationController
   def mypage
+    @user = User.all
     @resumes = Resume.all
     @advantages = Advantage.joins(:history).select("advantage, history_id, histories.title")
   end
