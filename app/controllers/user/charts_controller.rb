8d7db249e968d6eea569bc0e42173ab215f8145d
Masatooo0 <masato.n.0321@gmail.com>
2021-07-15 06:33:48 +0000

    [add]グラフ枠組み

--- app/controllers/user/charts_controller.rb
+++ app/controllers/user/charts_controller.rb
@@ -1,4 +1,8 @@
 class User::ChartsController < ApplicationController
   def index
+    @history = History.where(user_id: current_user.id).includes(:user)
+    gon.motivation = @history.pluck(:motivation).map(&:to_i)
+    @period = Period.joins(:histories).includes(:histories)
+    gon.data2 = @period.order(period: :DESC).pluck(:period)
   end
 end
