14edebc353941007e00b92baea266c689fc25360
Masatooo0 <masato.n.0321@gmail.com>
2021-07-17 01:33:11 +0000

    [add]ステータス変更

--- app/controllers/user/missions_controller.rb
+++ app/controllers/user/missions_controller.rb
@@ -18,11 +18,11 @@ class User::MissionsController < ApplicationController
     @mission.save
     redirect_to root_path
   end
-  
+
   def edit
     @mission = Mission.find(params[:id])
   end
-  
+
   def update
     @mission = Mission.find(params[:id])
     @mission.user_id = current_user.id
@@ -32,10 +32,10 @@ class User::MissionsController < ApplicationController
       render "edit"
     end
   end
-  
+
   private
 
   def mission_params
-    params.require(:mission).permit(:content, :deadline, :user_id)
+    params.require(:mission).permit(:content, :deadline, :status, :user_id)
   end
 end
