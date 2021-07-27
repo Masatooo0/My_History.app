c2304df940dee7f333f6d8504bab356f011cf1e5
Masatooo0 <masato.n.0321@gmail.com>
2021-07-27 13:52:51 +0000

    [add]showレイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_054923) do
+ActiveRecord::Schema.define(version: 2021_07_27_112029) do
 
   create_table "advantages", force: :cascade do |t|
     t.string "advantage"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_054923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "summarize"
+    t.string "image_id"
     t.index ["period_id"], name: "index_histories_on_period_id"
     t.index ["user_id"], name: "index_histories_on_user_id"
   end
