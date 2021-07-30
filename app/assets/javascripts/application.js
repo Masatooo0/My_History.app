dd31a0017c2a27f10226291dfbcd9c2266890cae
Masatooo0 <masato.n.0321@gmail.com>
2021-07-30 18:03:47 +0000

    [add]円グラフ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,19 +43,6 @@ $(document).on('turbolinks:load', function() {
 
 
 // missionタイマー機能
-
-function set2fig(num) {
-   // 数値が1桁だったら2桁の文字列にして返す
-   var ret;
-   if( num < 10 ) { ret = "0" + num; }
-   else { ret = num; }
-   return ret;
-}
-function isNumOrZero(num) {
-   // 数値でなかったら0にして返す
-   if( isNaN(num) ) { return 0; }
-   return num;
-}
 function showCountdown() {
    // 現在日時を数値(1970-01-01 00:00:00からのミリ秒)に変換
    var nowDate = new Date();
@@ -68,17 +55,8 @@ function showCountdown() {
    var inputHour  = document.getElementById("userHour").value;
    var inputMin   = document.getElementById("userMin").value;
    var inputSec   = document.getElementById("userSec").value;
-   var targetDate = new Date( isNumOrZero(inputYear), isNumOrZero(inputMonth), isNumOrZero(inputDate), isNumOrZero(inputHour), isNumOrZero(inputMin), isNumOrZero(inputSec) );
+   var targetDate = new Date(inputYear, inputMonth, inputDate, inputHour, inputMin, inputSec);
    var dnumTarget = targetDate.getTime();
-
-   // 表示を準備
-   var dlYear  = targetDate.getFullYear();
-   var dlMonth = targetDate.getMonth() + 1;
-   var dlDate  = targetDate.getDate();
-   var dlHour  = targetDate.getHours();
-   var dlMin   = targetDate.getMinutes();
-   var dlSec   = targetDate.getSeconds();
-   var msg1 = "期限の" + dlYear + "/" + dlMonth + "/" + dlDate + " " + set2fig(dlHour) + ":" + set2fig(dlMin) + ":" + set2fig(dlSec);
 
    // 引き算して日数(ミリ秒)の差を計算
    var diff2Dates = dnumTarget - dnumNow;
@@ -96,21 +74,10 @@ function showCountdown() {
    diff2Dates = diff2Dates % ( 1000 * 60 );
    var dSec   = diff2Dates / 1000;   // 秒
 
-   var msg2 = Math.floor(dDays) + "日"
+   var msg = Math.floor(dDays) + "日"
             + Math.floor(dHour) + "時間"
             + Math.floor(dMin) + "分"
             + Math.floor(dSec) + "秒";
-
-   // 表示文字列の作成
-   var msg;
-   if( dnumTarget > dnumNow ) {
-      // まだ期限が来ていない場合
-      msg =  "Mission終了まで、あと" + msg2 ;
-   }
-   else {
-      // 期限が過ぎた場合
-      msg = msg1 + "は、既に" + msg2 + "前に過ぎました。";
-   }
 
    // 作成した文字列を表示
    document.getElementById("RealtimeCountdownArea").innerHTML = msg;
@@ -133,5 +100,3 @@ $(document).on('turbolinks:load', function() {
     }
   });
 });
-
-
