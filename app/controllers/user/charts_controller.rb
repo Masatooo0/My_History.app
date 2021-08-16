9c14a883e333236c5fa514062d1d506cedfe70ea
Masatooo0 <masato.n.0321@gmail.com>
2021-08-16 05:30:44 +0000

    [fix]chart

--- app/controllers/user/charts_controller.rb
+++ app/controllers/user/charts_controller.rb
@@ -6,6 +6,6 @@ class User::ChartsController < ApplicationController
 
      #グラフY軸データ
     @history = History.where(user_id: current_user.id).includes(:user)
-    gon.motivation = @history.order(:period_id).pluck(:motivation)
+    gon.motivation = @history.order(:period_id).pluck(:motivation).map(&:to_i)
   end
 end
